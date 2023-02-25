740e58fd04ced88cdae744da7942c7fbd252b6a6
Matthew Craig <16930669+0x65-e@users.noreply.github.com>
2023-02-25 01:01:08 -0800

    Beginnings of dumpTree function to examine the parsed tree

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { CompositeGeneratorNode, NL, toString } from 'langium';
 import path from 'path';
-import { Ontology } from '../language-server/generated/ast';
+import { isRelationEntity, isSpecializableTerm, isVocabulary, Ontology, RelationEntity, Vocabulary, VocabularyStatement } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 
 export function generateJavaScript(model: Ontology, filePath: string, destination: string | undefined): string {
@@ -17,4 +17,45 @@ export function generateJavaScript(model: Ontology, filePath: string, destinatio
     }
     fs.writeFileSync(generatedFilePath, toString(fileNode));
     return generatedFilePath;
+}
+
+export function dumpTree(ontology: Ontology, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const generatedFilePath = `${path.join(data.destination, data.name)}.txt`;
+
+    const fileNode = new CompositeGeneratorNode();
+    fileNode.append(ontology.$type).appendNewLine();
+    if (isVocabulary(ontology)) {
+        let vocabulary: Vocabulary = ontology;
+        dumpStatements(vocabulary.ownedStatements, 1, fileNode);
+    } else {
+        fileNode.append("NOT VOCABULARY");
+    }
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+    return generatedFilePath;
+}
+
+function dumpStatements(stmts: VocabularyStatement[], level: number, printer: CompositeGeneratorNode): void {
+    for (let smt of stmts) {
+        printer.append(`${level} ${smt.$type}`).appendNewLine();
+        if (isRelationEntity(smt)) {
+            dumpRelationEntity(smt, printer)
+        }
+        if (isSpecializableTerm(smt)) {
+            if (smt.ownedSpecializations) smt.ownedSpecializations.forEach(spec => printer.append(`\tSpecializes ${spec.specializedTerm.$refText}`));
+        }
+    }
+}
+
+function dumpRelationEntity(entity: RelationEntity, printer: CompositeGeneratorNode): void {
+    if (entity.forwardRelation) {
+        printer.append(`\tForward Relation: ${entity.forwardRelation.name}`).appendNewLine();
+    }
+    if (entity.reverseRelation) {
+        printer.append(`\tReverse Relation: ${entity.reverseRelation.name}`).appendNewLine();
+    }
 }
