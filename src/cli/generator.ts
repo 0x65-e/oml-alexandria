43e00b9c7b1d694477a0aa65a248ab495ec5e69d
Matthew Craig <16930669+0x65-e@users.noreply.github.com>
2023-03-06 10:38:03 -0800

    Clean up dumpNodes function and print diagnostic information on the same line

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { AstNode, CompositeGeneratorNode, NL, toString } from 'langium';
 import path from 'path';
-import { isMember, isRelationEntity, isSpecializableTerm, isVocabulary, Ontology, Vocabulary } from '../language-server/generated/ast';
+import { isMember, isOntology, isRelationEntity, isSpecializableTerm, isVocabulary, Ontology } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 
 export function generateJavaScript(model: Ontology, filePath: string, destination: string | undefined): string {
@@ -24,13 +24,7 @@ export function dumpTree(ontology: Ontology, filePath: string, destination: stri
     const generatedFilePath = `${path.join(data.destination, data.name)}.txt`;
 
     const fileNode = new CompositeGeneratorNode();
-    fileNode.append(ontology.$type).appendNewLine();
-    if (isVocabulary(ontology)) {
-        let vocabulary: Vocabulary = ontology;
-        if (vocabulary.ownedStatements) vocabulary.ownedStatements.forEach(stmt => dumpNodes(stmt, 1, fileNode));
-    } else {
-        fileNode.append("NOT VOCABULARY");
-    }
+    dumpNodes(ontology, 0, fileNode);
 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
@@ -40,16 +34,35 @@ export function dumpTree(ontology: Ontology, filePath: string, destination: stri
 }
 
 function dumpNodes(smt: AstNode, level: number, printer: CompositeGeneratorNode): void {
+    // Print the type name of this node
     printer.append('\t'.repeat(level) + `${smt.$type}`);
+
+    // Print diagnostic information for selected types, if available
+    let diag: string[] = [];
+
+    // An `Ontology` has a globally-unique namespace
+    if (isOntology(smt)) {
+        diag.push(`namespace: ${smt.namespace}`);
+    }
+    // A `Member` is an element identified by a name
     if (isMember(smt)) {
-        printer.append(` (INFO: name: ${smt.name})`);
+        diag.push(`name: ${smt.name}`);
+        // A `SpecializableTerm` can specialize other `Term`s
+        if (isSpecializableTerm(smt)) {
+            if (smt.ownedSpecializations) smt.ownedSpecializations.forEach(spec => diag.push(`specializes: ${spec.specializedTerm.$refText}`));
+        }
+    }
+
+    if (diag.length > 0) {
+        printer.append(` (${diag.join(", ")})`);
     }
     printer.appendNewLine();
-    if (isRelationEntity(smt)) {
+
+    // Recurse on child nodes
+    if (isVocabulary(smt)) {
+        if (smt.ownedStatements) smt.ownedStatements.forEach(s => dumpNodes(s, level+1, printer));
+    } else if (isRelationEntity(smt)) {
         if (smt.forwardRelation) dumpNodes(smt.forwardRelation, level+1, printer);
         if (smt.reverseRelation) dumpNodes(smt.reverseRelation, level+1, printer);
-    }
-    if (isSpecializableTerm(smt)) {
-        if (smt.ownedSpecializations) smt.ownedSpecializations.forEach(spec => printer.append('\t'.repeat(level+1) + `(INFO: specializes: ${spec.specializedTerm.$refText})`).appendNewLine());
     }
 }
