feb0e9da2103f74f13e36ac833c707fa5fe088a0
Matthew Craig <16930669+0x65-e@users.noreply.github.com>
2023-03-07 09:38:25 -0800

    Warn on duplicate specializations

--- src/language-server/oml-validator.ts
+++ src/language-server/oml-validator.ts
@@ -9,8 +9,8 @@ export function registerValidationChecks(services: OmlServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.OmlValidator;
     const checks: ValidationChecks<OmlAstType> = {
-        SpecializableTerm: validator.checkSpecializationTypesMatch,
-        SpecializableTermReference: validator.checkReferenceSpecializationTypeMatch
+        SpecializableTerm: [validator.checkSpecializationTypesMatch, validator.checkDuplicateSpecializations],
+        SpecializableTermReference: [validator.checkReferenceSpecializationTypeMatch, validator.checkReferenceDuplicateSpecializations]
     };
     registry.register(checks, validator);
 }
@@ -37,11 +37,28 @@ export class OmlValidator {
         }
     }
 
-    checkReferenceSpecializationTypeMatch(specRef: SpecializableTermReference, accept: ValidationAcceptor): void {
-        if (!isSpecializableTermReference(specRef)) {
-            throw new Error('Expected a SpecializableTermReference in validation but got the wrong type');
+    checkDuplicateSpecializations(specTerm: SpecializableTerm, accept: ValidationAcceptor): void {
+        if (!isSpecializableTerm(specTerm)) {
+            throw new Error('Expected a SpecializableTerm in validation but got the wrong type');
         }
 
+        if (specTerm.ownedSpecializations) {
+            const reported = new Set();
+            specTerm.ownedSpecializations.forEach(spec => {
+                if (spec.specializedTerm.ref) {
+                    if (spec.specializedTerm.ref.name == specTerm.name) {
+                        accept('warning', `${specTerm.name} specializes itself`, {node: specTerm, property: 'ownedSpecializations'});
+                    }
+                    if (reported.has(spec.specializedTerm.ref.name)) {
+                        accept('warning', `${specTerm.name} specializes ${spec.specializedTerm.ref.name} twice`, {node: specTerm, property: 'ownedSpecializations'});
+                    }
+                    reported.add(spec.specializedTerm.ref.name);
+                }
+            })
+        }
+    }
+
+    extractSpecializableTermFromReference(specRef: SpecializableTermReference) : SpecializableTerm | null {
         // Extract the SpecializableTerm from the reference
         let specTerm: SpecializableTerm | null = null;
         if (isFacetedScalarReference(specRef) || isEnumeratedScalarReference(specRef)) {
@@ -59,6 +76,15 @@ export class OmlValidator {
         } else {
             throw new Error('Unknown subtype of SpecializableTermReference');
         }
+        return specTerm
+    }
+
+    checkReferenceSpecializationTypeMatch(specRef: SpecializableTermReference, accept: ValidationAcceptor): void {
+        if (!isSpecializableTermReference(specRef)) {
+            throw new Error('Expected a SpecializableTermReference in validation but got the wrong type');
+        }
+
+        const specTerm = this.extractSpecializableTermFromReference(specRef);
 
         if (specRef.ownedSpecializations) {
             specRef.ownedSpecializations.forEach(spec => {
@@ -67,6 +93,29 @@ export class OmlValidator {
                     if (!(spec.specializedTerm.ref.$type == specTerm.$type || (isEntity(specTerm) && isAspect(spec.specializedTerm.ref)))) {
                         accept('error', `${specTerm.name} is of type ${specTerm.$type} but is trying to specialize ${spec.specializedTerm.ref.name} of type ${spec.specializedTerm.ref.$type}`, {node: specRef, property: 'ownedSpecializations'});
                     }
+                }
+            })
+        }
+    }
+
+    checkReferenceDuplicateSpecializations(specRef: SpecializableTermReference, accept: ValidationAcceptor): void {
+        if (!isSpecializableTermReference(specRef)) {
+            throw new Error('Expected a SpecializableTermReference in validation but got the wrong type');
+        }
+
+        const specTerm = this.extractSpecializableTermFromReference(specRef);
+
+        if (specRef.ownedSpecializations) {
+            const reported = new Set();
+            specRef.ownedSpecializations.forEach(spec => {
+                if (spec.specializedTerm.ref && specTerm) {
+                    if (spec.specializedTerm.ref.name == specTerm.name) {
+                        accept('warning', `${specTerm.name} specializes itself`, {node: specRef, property: 'ownedSpecializations'});
+                    }
+                    if (reported.has(spec.specializedTerm.ref.name)) {
+                        accept('warning', `${specTerm.name} specializes ${spec.specializedTerm.ref.name} twice`, {node: specRef, property: 'ownedSpecializations'});
+                    }
+                    reported.add(spec.specializedTerm.ref.name);
                 }
             })
         }
